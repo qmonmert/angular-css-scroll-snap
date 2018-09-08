db755c5df852e0cb5a23f3dcbe1111392d4c6c1d
Quentin Monmert <qmonmert@qmonmert.home>
2018-09-08 19:09:41 +0200

    scroll-snap :heart_eyes:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ScrollSnapComponent } from './scroll-snap/scroll-snap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScrollSnapComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/scroll-snap/scroll-snap.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scroll-snap',
+  templateUrl: './scroll-snap.component.html',
+  styleUrls: ['./scroll-snap.component.css']
+})
+export class ScrollSnapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
